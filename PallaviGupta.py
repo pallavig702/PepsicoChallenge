37007ab8affc6486ba8124aec16b824d84c52269
pallavig702 <31351096+pallavig702@users.noreply.github.com>
2024-04-10 18:01:23 -0500

    Update PallaviGupta.py

--- PallaviGupta.py
+++ PallaviGupta.py
@@ -74,7 +74,9 @@ Test_df=dataset.drop(["StudyNumber","Prediction","TransparentWindowinPackage","P
 df.shape
 
 
-
+#########################################################################################
+############################### SOME EXPLORATIONS  ######################################
+#########################################################################################
 yes_no_columns_df = list(filter(lambda i: df[i].dtype!=np.float64, df.columns))
 yes_no_columns_Test_df = list(filter(lambda i: Test_df[i].dtype!=np.float64, Test_df.columns))
 def PrintUniqueValues(yes_no_column,datase):
@@ -93,67 +95,45 @@ for column_name in yes_no_columns_Test_df:
     print('Filling missing values of {} with {}'.format(column_name, mode))
     Test_df[column_name].fillna(mode, inplace=True)
 
+#dfObj2 = pd.DataFrame(df, columns = cols)
+#print("Nan in each columns" , dfObj2.isnull().sum(), sep='\n')
+#PrintUniqueValues(yes_no_columns_df,df)
 
-# In[50]:
-
-
-dfObj2 = pd.DataFrame(df, columns = cols)
-print("Nan in each columns" , dfObj2.isnull().sum(), sep='\n')
-PrintUniqueValues(yes_no_columns_df,df)
-
-
-# In[51]:
-
-
+#########################################################################################
+########################### SPLIT DATA INTO TRAIN AND TEXT# ############################
+#########################################################################################
 X=df.iloc[:,df.columns!='BinaryDifferenceFromFresh']
 y=df.BinaryDifferenceFromFresh
 
-
-# In[52]:
-
-
-
-df2 = pd.get_dummies(X)
-df2.shape
-
-
-# In[53]:
-
-
-
+#df2 = pd.get_dummies(X)
+#df2.shape
 #X=df2.iloc[:,df.columns != 'BinaryDifferenceFromFresh']
 X_train, X_test, y_train, y_test = train_test_split(df2,y,test_size=0.33, random_state=42)
 
-
-# In[54]:
-
-
-
-
-class_weight=dict({1:1.9, 0:20})
+#########################################################################################
+##################################### DEFINE CLASSIFIER #################################
+#########################################################################################
+#class_weight=dict({1:1.9, 0:20}) #or "balanced"
 classifier2= RandomForestClassifier(n_estimators=50, max_depth=16,random_state=0, n_jobs=2)#,class_weight=class_weight)
 
-
-# In[55]:
-
-
+##########################################################################################
+################################## MODEL FITTING AND TEST R###############################
+##########################################################################################
 classifier2.fit(X_train,y_train)
 y_pred=classifier2.predict(X_test)
 accuracy_score(y_test,y_pred)
 
-
-# In[56]:
-
-
-
+##########################################################################################
+################################## PRINT MODEL METRICS ###################################
+##########################################################################################
 print("Confusion Matrix:===> ","\n", confusion_matrix(y_test, y_pred),"\n")
 print("Classification Report:===> ","\n", classification_report(y_test, y_pred))
 print("Best Accuracy Score:===> ", accuracy_score(y_test, y_pred))
 
 
-# In[57]:
-
-
+##############################################################################################
+############################ MAKE PREDICTION FOR COMPETITION #################################
+##############################################################################################
 Sampleid=Test_df.SampleID
 new=Test_df.drop('SampleID',axis=1)
 X_Test_df=new.iloc[:,:-1]
@@ -161,16 +141,8 @@ X_onehot=pd.get_dummies(X_Test_df)
 y_Test_df=new.BinaryDifferenceFromFresh
 X_Test_df.head()
 
-
-# In[58]:
-
-
 pred=classifier2.predict(X_onehot)
 accuracy_score(y_Test_df,pred)
-
-
-# In[59]:
-
 
 predictions={}
 actual={}
@@ -180,33 +152,12 @@ for i in range(Test_df.shape[0]):
 
 olddataset["pred"]=0
 
-olddataset.head()
-
-
-# In[60]:
-
-
-#for keys,values in predictions.items():
-#    print(keys,values)
-
-
-# In[61]:
-
-
 for i in range(olddataset.shape[0]):
     sampleid=olddataset.iloc[i,1]
     olddataset.pred[i]=predictions[sampleid]
     
-    #print(i)
 olddataset["Prediction"]=olddataset["pred"]
-
 olddataset.head()
-    
-
-
-# In[64]:
-
-
 final=olddataset.drop(['pred','BinaryDifferenceFromFresh'],axis=1)
 final.to_excel("outputshelf_life_file_with_predictions.xlsx")
 
